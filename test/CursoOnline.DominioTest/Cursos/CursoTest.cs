26f0b3fc6875ed54d02e5351aa511219b4cb84d7
Gabriel Sanga <gabriel.sanga.fef@gmail.com>
2023-02-04 19:59:25 -0300

    Iniciando caso de teste de curso

---
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ExpectedObjects;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            var oCursoEsperado = new
+            {
+                nome = "Informatica Basica",
+                cargaHoraria = (double) 40,
+                publicoAlvo = PublicoAlvo.Estudante,
+                valor = (double) 950.00
+            };
+
+            var oCurso = new Curso(oCursoEsperado.nome, oCursoEsperado.cargaHoraria, oCursoEsperado.publicoAlvo, oCursoEsperado.valor);
+
+            oCursoEsperado.ToExpectedObject().ShouldMatch(oCurso);
+        }
+
+    }
+
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreendedor
+    }
+
+    internal class Curso
+    {
+
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        {
+            this.nome = nome;
+            this.cargaHoraria = cargaHoraria;
+            this.publicoAlvo = publicoAlvo;
+            this.valor = valor;
+        }
+
+        public string nome { get; private set; }
+        public double cargaHoraria { get; private set; }
+        public PublicoAlvo publicoAlvo { get; private set; }
+        public double valor { get; private set; }
+    }
+}
